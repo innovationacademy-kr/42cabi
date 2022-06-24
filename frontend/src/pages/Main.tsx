4903d0e740068ff69664847ae8b2dfcd1d5b894e
윤현지 <hyoon@student.42seoul.kr>
2022-06-25 01:30:16 +0900

    :test_tube: test redirection

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -10,12 +10,12 @@ export default function Main() {
         <img src="../img/logo.png" alt="logo" />
       </div>
       <div className="row d-grid gap-2 col-6 mx-auto">
-				{/* <a className="btn btn-lg" id="loginBtn" href={url}>
-          L O G I N
-        </a> */}
 				<a className="btn btn-lg" id="loginBtn" href={url}>
-          G O !
+          L O G I N
         </a>
+				{/* <a className="btn btn-lg" id="loginBtn" href={url}>
+          G O !
+        </a> */}
       </div>
     </div>
   );
