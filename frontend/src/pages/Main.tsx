f9b3a8892ab7ab876f38c0ddf5d14b2bbda83502
윤현지 <hyoon@student.42seoul.kr>
2022-06-25 01:26:08 +0900

    :test_tube: test redirection

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,7 +1,8 @@
 import "./main.css";
 
 export default function Main() {
-  const url: string = "/auth/login";
+  // const url: string = "/auth/login";
+  const url: string = "https://cabi.42cadet.kr";
 
   return (
     <div className="container">
@@ -9,8 +10,11 @@ export default function Main() {
         <img src="../img/logo.png" alt="logo" />
       </div>
       <div className="row d-grid gap-2 col-6 mx-auto">
-				<a className="btn btn-lg" id="loginBtn" href={url}>
+				{/* <a className="btn btn-lg" id="loginBtn" href={url}>
           L O G I N
+        </a> */}
+				<a className="btn btn-lg" id="loginBtn" href={url}>
+          G O !
         </a>
       </div>
     </div>
